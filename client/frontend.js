e376c1f25218da540a53ac39d283d799c624a42c
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2022-02-27 15:17:54 +0300

    my restfull api

---
+++ client/frontend.js
@@ -0,0 +1,81 @@
+// import { createApp } from 'vue'
+//
+// createApp({
+//     data() {
+//         return {
+//             message: 'Hello Vue!'
+//         }
+//     }
+// }).mount('#app')
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+Vue.component('loader',{
+    template: `<div style="display: flex; justify-content: center; align-items: center">
+<div class="spinner-border" role="status">
+  <span class="visually-hidden">Загрузка...</span>
+</div>
+</div>`
+})
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [],
+            loading: false
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.name.trim() && this.form.name.trim()
+        }
+    },
+    methods: {
+        async createContact() {
+            const {...contact} = this.form
+            const newContact = await  request('/api/contacts', 'POST', contact)
+            this.contacts.push(newContact)
+            this.form.name = ''
+            this.form.value = ''
+        },
+        async markContact(id) {
+            const contact = this.contacts.find((c) => {
+                return c.id === id
+            })
+            console.log('client id', id)
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {...contact, marked: true})
+            contact.marked = updated.marked
+        },
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE')
+            this.contacts = this.contacts.filter(c => c.id !== id)
+        },
+    },
+    async mounted(){
+        this.loading = true
+       this.contacts = await request('/api/contacts')
+        this.loading = false
+    },
+})
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+        if(data){
+            headers['Content-Type'] = 'application/json';
+            body = JSON.stringify(data)
+        }
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (e) {
+console.warn('error', e.message)
+    }
+
+}
